6024bab194c68fb1f66e2d537482979302502da4
dominthomas <dominthomas@gmail.com>
2019-11-21 17:16:29 +0000

    Extract 3Tesla 2D slices containing brain tissue only

--- 2D_Extraction.R
+++ 2D_Extraction.R
@@ -1,45 +1,61 @@
-library(dplyr)
 library(neurobase)
+library(imager)
+library(png)
+library(parallel)
+library(doParallel)
+library(stringr)
 
-setwd("/home/dthomas/AD")
+setwd("/home/dthomas/AD/")
 
-ad <- list.files("extracted_ad/")
-cn <- list.files("extracted_cn")
+ad <- list.files("3T_extracted_ad/")
+cn <- list.files("3T_extracted_cn/")
 
+###########################################TEST################################################
 
+nifti_file <- readnii("3T_extracted_ad/sub-OAS30024_ses-d0084_T1wBrainExtractionBrain.nii.gz")
+nifti_file <- "sub-OAS30024_ses-d0084_T1wBrainExtractionBrain.nii.gz"
 
-setwd("/home/dthomas/AD/extracted_ad")
-nifti1 <- readnii("sub-OAS30091_ses-d0092_run-01_T1wBrainExtractionBrain.nii.gz")
-nifti2 <- readnii("sub-OAS30144_ses-d1204_run-01_T1wBrainExtractionBrain.nii.gz")
-nifti3 <- readnii("sub-OAS30206_ses-d0306_run-02_T1wBrainExtractionBrain.nii.gz")
-nifti4 <- readnii("sub-OAS30144_ses-d1204_run-03_T1wBrainExtractionBrain.nii.gz")
-niftis <- append(niftis, list(nifti1))
-niftis <- append(niftis, list(nifti2))
-niftis <- append(niftis, list(nifti3))
-
-niftis <- list()
-for(file in ad)
-{
-  # Initially, read in 50 CN images.
-  if(length(niftis) == 5)
-  {
-    break
-  }
-  nifti_file <- readnii(file)
-  bob <- as.array(dim(nifti_file))
-  bsum <- sum(match(c(176, 256, 256), bob))
-  if(!is.na(bsum) && bsum != 5)
-  {
-    niftis <- append(niftis, list(nifti_file))
-  }
-}
-
-image(nifti, z = 145, plot.type="single", plane="coronal")
-image(nifti4, z = 100, plot.type="single", plane="sagittal")
-
-
-###############################################################################################
 png(filename = "/home/dthomas/AD/teset.png")
-image(ad_test1, z = 145, plot.type="single", plane="coronal")
+image(nifti1, z = 120, plot.type="single", plane="coronal")
 dev.off()
 
+t1 <- readPNG("/home/dthomas/AD/teset.png")
+
+t1_nifti <- load.image("/home/dthomas/AD/teset.png")
+plot(t1_nifti)
+###############################################################################################
+
+setwd("/home/dthomas/AD/3T_extracted_ad/")
+
+registerDoParallel(8)
+
+image_has_pixels_over_zero <- function(file_path)
+{
+  img <- readPNG(file_path)
+  return(any(img > 0))
+}
+
+foreach(nifti_file = ad) %dopar%
+  {
+    t1 <- readnii(paste(nifti_file))
+    sub <- str_extract(nifti_file, '(OAS\\d*)')
+    ses <- str_extract(nifti_file, '(d\\d*)')
+    folder <- paste("/home/dthomas/AD/2D/AD/", sub, "_", ses, sep="")
+    dir.create(folder)
+    
+    for(slice_num in 1:256)
+    {
+      png(filename = paste(folder, "/", slice_num, ".png", sep=""))
+      image(t1, z = slice_num, plot.type="single", plane="coronal")
+      dev.off()     
+      
+      if(image_has_pixels_over_zero(paste(folder, "/", slice_num, ".png", sep="")))
+      {
+        next
+      }
+      else
+      {
+        file.remove(paste(folder, "/", slice_num, ".png", sep=""))
+      }
+    }
+  }
