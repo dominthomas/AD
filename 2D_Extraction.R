1678203a4e2a44c1669de8dd250dd3dde5a2edc6
dominthomas <dominthomas@gmail.com>
2019-11-22 07:14:57 +0000

    Extracted 2D slices

--- 2D_Extraction.R
+++ 2D_Extraction.R
@@ -6,28 +6,10 @@ library(doParallel)
 library(stringr)
 
 setwd("/home/dthomas/AD/")
+registerDoParallel(16)
 
 ad <- list.files("3T_extracted_ad/")
 cn <- list.files("3T_extracted_cn/")
-
-###########################################TEST################################################
-
-nifti_file <- readnii("3T_extracted_ad/sub-OAS30024_ses-d0084_T1wBrainExtractionBrain.nii.gz")
-nifti_file <- "sub-OAS30024_ses-d0084_T1wBrainExtractionBrain.nii.gz"
-
-png(filename = "/home/dthomas/AD/teset.png")
-image(nifti1, z = 120, plot.type="single", plane="coronal")
-dev.off()
-
-t1 <- readPNG("/home/dthomas/AD/teset.png")
-
-t1_nifti <- load.image("/home/dthomas/AD/teset.png")
-plot(t1_nifti)
-###############################################################################################
-
-setwd("/home/dthomas/AD/3T_extracted_ad/")
-
-registerDoParallel(8)
 
 image_has_pixels_over_zero <- function(file_path)
 {
@@ -35,27 +17,97 @@ image_has_pixels_over_zero <- function(file_path)
   return(any(img > 0))
 }
 
-foreach(nifti_file = ad) %dopar%
+
+setwd("/home/dthomas/AD/3T_extracted_ad/")
+start_time_ad <- Sys.time()
+foreach(nifti_file_name = ad) %dopar%
   {
-    t1 <- readnii(paste(nifti_file))
-    sub <- str_extract(nifti_file, '(OAS\\d*)')
-    ses <- str_extract(nifti_file, '(d\\d*)')
-    folder <- paste("/home/dthomas/AD/2D/AD/", sub, "_", ses, sep="")
+    t1 <- readnii(paste(nifti_file_name))
+    sub <- str_extract(nifti_file_name, '(OAS\\d*)')
+    ses <- str_extract(nifti_file_name, '(d\\d*)')
+    run <- str_extract(nifti_file_name, '(run-\\d*)')
+    folder <- paste("/home/dthomas/AD/2D/AD/", sub, "_", ses, run, sep="")
     dir.create(folder)
     
-    for(slice_num in 1:256)
+    for(slice_num in 1:dim(t1)[2])
     {
-      png(filename = paste(folder, "/", slice_num, ".png", sep=""))
+      full_path <- paste(folder, "/", slice_num, ".png", sep="")
+      png(filename = full_path)
       image(t1, z = slice_num, plot.type="single", plane="coronal")
       dev.off()     
       
-      if(image_has_pixels_over_zero(paste(folder, "/", slice_num, ".png", sep="")))
+      if(image_has_pixels_over_zero(full_path))
       {
         next
       }
       else
       {
-        file.remove(paste(folder, "/", slice_num, ".png", sep=""))
+        file.remove(full_path)
       }
     }
   }
+end_time_ad <- Sys.time()
+
+gc()
+start_time_cn <- Sys.time()
+setwd("/home/dthomas/AD/3T_extracted_cn/")
+foreach(nifti_file_name = cn) %dopar%
+  {
+    t1 <- readnii(paste(nifti_file_name))
+    sub <- str_extract(nifti_file_name, '(OAS\\d*)')
+    ses <- str_extract(nifti_file_name, '(d\\d*)')
+    run <- str_extract(nifti_file_name, '(run-\\d*)')
+    folder <- paste("/home/dthomas/AD/2D/CN/", sub, "_", ses, run, sep="")
+    dir.create(folder)
+    
+    for(slice_num in 1:dim(t1)[2])
+    {
+      full_path <- paste(folder, "/", slice_num, ".png", sep="")
+      png(filename = full_path)
+      image(t1, z = slice_num, plot.type="single", plane="coronal")
+      dev.off()     
+      
+      if(image_has_pixels_over_zero(full_path))
+      {
+        next
+      }
+      else
+      {
+        file.remove(full_path)
+      }
+    }
+  }
+end_time_cn <- Sys.time()
+
+print(end_time_cn - start_time_cn)
+
+
+
+# An oddity, there are only 1640 directories, ther should be 1641.
+
+cn_2D <- list.files("/home/dthomas/AD/2D/CN/")
+nifti_names <- c()
+for(nifti_file_name in cn) 
+  {
+    sub <- str_extract(nifti_file_name, '(OAS\\d*)')
+    ses <- str_extract(nifti_file_name, '(d\\d*)')
+    run <- str_extract(nifti_file_name, '(run-\\d*)')
+    
+    folder_name <- paste(sub, "_", ses, run, sep="")
+    nifti_names <- c(nifti_names, folder_name)
+}
+setdiff(nifti_names, cn_2D)
+###########################################TEST################################################
+
+nifti_file <- readnii("3T_extracted_ad/sub-OAS30024_ses-d0084_T1wBrainExtractionBrain.nii.gz")
+nifti_file_name <- "sub-OAS30024_ses-d0084_T1wBrainExtractionBrain.nii.gz"
+
+png(filename = "/home/dthomas/AD/teset.png")
+image(nifti_file, z = 240, plot.type="single", plane="coronal")
+dev.off()
+
+t1 <- readPNG("/home/dthomas/AD/teset.png")
+
+t1_nifti <- load.image("/home/dthomas/AD/teset.png")
+plot(t1_nifti)
+###############################################################################################
